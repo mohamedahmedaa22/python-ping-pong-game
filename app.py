02c7d16467fb1bbb2c5b67dc85b3065bd43846c1
Mohammed Abdellatef <50413981+mohamedahmedaa22@users.noreply.github.com>
2022-02-04 16:46:24 +0200

    player 1 to the screen

--- app.py
+++ app.py
@@ -9,5 +9,18 @@ wind.setup(width=800, height=600)
 
 wind.tracer(0) # stop update the screen
 
+
+#player 1
+player1 = turtle.Turtle()
+player1.speed(0)
+player1.shape("square")
+player1.color("blue")
+player1.shapesize(stretch_wid=5, stretch_len=1)
+player1.penup()
+player1.goto(-350, 0)
+#player 2
+
+#ball
+
 while True:
     wind.update()
